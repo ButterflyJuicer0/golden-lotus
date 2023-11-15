ea7a2e090872deac803e6eea778402809ea02d78
ButterflyJuicer <142724516+ButterflyJuicer0@users.noreply.github.com>
2023-11-15 18:14:33 +0800

    Delete golden-lotus-server directory
    "Remove unnecessary components"

--- golden-lotus-server/src/main/java/com/nus/service/CategoryService.java
+++
@@ -1,9 +0,0 @@
-package com.nus.service;
-
-import com.nus.pojo.vo.CategoryVO;
-
-import java.util.List;
-
-public interface CategoryService {
-    List<CategoryVO> showAll();
-}
--- golden-lotus-server/src/main/java/com/nus/service/CommonService.java
+++
@@ -1,8 +0,0 @@
-package com.nus.service;
-
-import com.nus.pojo.dto.AccountLoginDTO;
-import com.nus.pojo.entity.People;
-
-public interface CommonService {
-    People login(AccountLoginDTO accountLoginDTO);
-}
